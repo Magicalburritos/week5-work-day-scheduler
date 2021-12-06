6577d71f087706d9310654473fcd7cde01dcd7e6
joe <hudakjoe@msn.com>
2021-12-05 21:08:21 -0500

    got local storage to work

--- Develop/javascript.js
+++ Develop/javascript.js
@@ -1,30 +1,30 @@
 // global vars selectors
-
+var taskItems = [];
 // local storage var
 $('#currentDay').html(moment().format('dddd MMM Do'));
 // btn save on click to localstorage
 
-var saveTasks = function (tasks) {
-  localStorage.setItem('tasks', JSON.stringify(tasks));
+var saveTasks = function (tasks, id) {
+  localStorage.setItem(id, JSON.stringify(tasks));
+  console.log(document.getElementById('d1').value);
 };
 $('i').on('click', function () {
   var id = $(this).attr('id');
   var task = $(this).parent().prev('.col-lg-8').children('input').val();
-  //   task = 'hi';
+
   saveTasks(task, id);
   console.log(localStorage.getItem('tasks'));
 });
 // time change color
 
 var loadTasks = function () {
-  //   for (let i = 1; i <= 9; i++) {
-  //     console.log(i);
-  //     var row = document
-  //       .getElementById('row-' + i)
-  //       .getElementsByClassName('description');
-  //     row.value = localStorage.getItem(i);
-  //     console.log(localStorage.getItem(i));
-  //   }
+  for (let i = 1; i <= 9; i++) {
+    console.log(i);
+    document.getElementById('d' + i).value = JSON.parse(
+      localStorage.getItem(i)
+    );
+    console.log(localStorage.getItem(i));
+  }
   console.log(localStorage.getItem('tasks'));
 };
 
