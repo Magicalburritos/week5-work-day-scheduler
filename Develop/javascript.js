b289460cb3ac301439b7ae9d70125c4e1b249098
joe <hudakjoe@msn.com>
2021-12-05 20:48:49 -0500

    working thru some things

--- Develop/javascript.js
+++ Develop/javascript.js
@@ -1,7 +1,31 @@
 // global vars selectors
+
+// local storage var
 $('#currentDay').html(moment().format('dddd MMM Do'));
 // btn save on click to localstorage
 
+var saveTasks = function (tasks) {
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+};
+$('i').on('click', function () {
+  var id = $(this).attr('id');
+  var task = $(this).parent().prev('.col-lg-8').children('input').val();
+  //   task = 'hi';
+  saveTasks(task, id);
+  console.log(localStorage.getItem('tasks'));
+});
 // time change color
 
-//
+var loadTasks = function () {
+  //   for (let i = 1; i <= 9; i++) {
+  //     console.log(i);
+  //     var row = document
+  //       .getElementById('row-' + i)
+  //       .getElementsByClassName('description');
+  //     row.value = localStorage.getItem(i);
+  //     console.log(localStorage.getItem(i));
+  //   }
+  console.log(localStorage.getItem('tasks'));
+};
+
+loadTasks();
