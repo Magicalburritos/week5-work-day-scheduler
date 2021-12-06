d7813f16cb7bf982aa4975db836fe17a009b7eb4
joe <hudakjoe@msn.com>
2021-12-05 23:03:57 -0500

    added css

--- Develop/javascript.js
+++ Develop/javascript.js
@@ -1,23 +1,29 @@
 // global vars selectors
 var currentTime;
+
+// helper function
+
 var displayTime = function () {
   currentTime = moment().format('HH');
 };
-// btn save on click to localstorage
-$('#currentDay').html(moment().format('dddd MMM Do'));
+
 var saveTasks = function (tasks, id) {
   localStorage.setItem(id, JSON.stringify(tasks));
 };
+
+// on click save to storage
 $('i').on('click', function () {
   var id = $(this).attr('id');
-  var task = $(this).parent().prev('.col-lg-8').children('input').val();
+  var description = $('#d' + id).val();
 
-  saveTasks(task, id);
+  saveTasks(description, id);
 });
+
 // time change color
 var changeColors = function () {
   for (let i = 9; i <= 17; i++) {
     var input = $('#d' + i);
+
     if (currentTime > i) {
       input.addClass('past');
     }
@@ -38,13 +44,17 @@ var loadTasks = function () {
   }
 };
 
-loadTasks();
-
+// main function
 $(document).ready(function () {
+  $('#currentDay').html(moment().format('dddd MMM Do'));
   displayTime();
+  loadTasks();
   changeColors();
+
+  // continued check every 1 min
   window.setInterval(function () {
+    $('#currentDay').html(moment().format('dddd MMM Do'));
     displayTime();
     changeColors();
-  }, 1000);
+  }, 60000);
 });
